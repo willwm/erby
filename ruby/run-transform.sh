eeb0575bbfe83ff34224be7d456c7b1f3af68829
Will Wolff-Myren <willwm@gmail.com>
2024-06-10 20:31:51 -0700

    Fix output_directory

--- ruby/run-transform.sh
+++ ruby/run-transform.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 input_directory="templates"
-output_directory="../output"
+output_directory="transformed"
 
 if [ -f ../set-env.sh ]; then
     echo "Sourcing ../set-env.sh"
     source ../set-env.sh
 fi
 
-rm -rf transformed
+rm -rf $output_directory
 
 ruby ./transform_templates.rb -i $input_directory -o $output_directory
 
